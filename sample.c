d4208fecbe5552420052c565d46d3a0fe2babb36
Sruthimol-Kurian <sruthimolkurian@amaljyothi.ac.in>
2019-03-06 17:47:28 +0530

    sample.c file updated again

--- sample.c
+++ sample.c
@@ -4,10 +4,8 @@
 public static void main(){
 	int a;
 	printf("Hello World\n");
-<<<<<<< HEAD
+
 printf("This is a sample C program\n");
-=======
-	
->>>>>>> ca1602295142c35d12d80958a8140f0224bd2ca2
+
 }
 
